141e9e7c3ef8305cdc8f2422001cd816bae166f2
Tim <timbo.jacquet@gmail.com>
2020-07-09 16:12:16 +0200

    add properties container

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Agency from "./components/Agency/Agency";
-import Properties from "./components/Properties/Properties";
+import PropertiesContainer from "./containers/PropertiesContainer";
 import ProperyHome from "./components/PropertyForm/PropertyForm";
 import "./App.css";
 import HomeContainer from "./containers/HomeContainer";
@@ -15,7 +15,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={HomeContainer} />
           <Route path="/agency" component={Agency} />
-          <Route path="/properties" component={Properties} />
+          <Route path="/properties" component={PropertiesContainer} />
           <Route path="/propertyHome" component={ProperyHome} />
           <Route>
             <p>Page not found</p>
--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import Property from "../Home/Property";
 
 class Properties extends Component {
   render() {
-    return <div>PROPRIETES</div>;
+    const { properties } = this.props;
+    const propertiesJsx = properties.map((p) => (
+      <article key={p.id} class="property">
+        <Property properties={p} />
+      </article>
+    ));
+    return (
+      <>
+        <div className="page-title">PROPRIETES</div>
+        <div className="properties">{propertiesJsx}</div>
+      </>
+    );
   }
 }
 
