e5de1fd3e7cd3e8463eb4a3d3873e40b15996734
Marc <kemar53@hotmail.fr>
2020-07-10 16:58:42 +0200

    Informations details display or not

--- src/components/Properties/PropertyDetail.js
+++ src/components/Properties/PropertyDetail.js
@@ -7,6 +7,11 @@ class PropertyDetail extends Component {
     if (!property) {
       return <p>Chargement en cours..</p>;
     }
+    let details = 'Pas d\'information supplémentaire';
+    if (property.features.properties != undefined){
+      details = 'Détails supplémentaires: '+property.features.properties;
+    }
+    
     return (
       <section className="container">
 <div className="card">
@@ -21,7 +26,7 @@ class PropertyDetail extends Component {
         <ul className="list-group list-group-flush">
           <li className="list-group-item">Prix: {property.price} € | Quartier: {property.district.name} | Date d'ajout du bien: {property.createdAt.substring(0, 10)} | 
           Catégorie: {property.category.name} | Surface: {property.surface} m2 | Nombre de pièces: {property.bedroom}</li>
-          <li className="list-group-item">Détails supplémentaires: {property.features.properties}</li>
+          <li className="list-group-item">{details}</li>
         </ul>
         </div>
       </section>
