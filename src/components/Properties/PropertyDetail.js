1ab2a340faac3da0912669a09cbc3bc232577f92
Marc <kemar53@hotmail.fr>
2020-07-10 16:50:24 +0200

    design detail image

--- src/components/Properties/PropertyDetail.js
+++ src/components/Properties/PropertyDetail.js
@@ -8,30 +8,22 @@ class PropertyDetail extends Component {
       return <p>Chargement en cours..</p>;
     }
     return (
-      <div>
+      <section className="container">
+<div className="card">
         <img
           src={'http://lojeris.api.pierre-jehan.com/uploads/'+ property.picture.filePath}
           alt=""
           className="card-img-top"
         />
-
-        <div className="price">
-          <div className="tag">Prix: {property.price} €</div>
+        <div className="card-body">
+          <p className="card-text">Description: {property.description}</p>
         </div>
-        <div className="more-info">
-          <div className="info-line">Quartier: {property.district.name}</div>
-          <div className="info-line">
-            Date d'ajout du bien: {property.createdAt.substring(0, 10)}
-          </div>
-          <div className="info-line">Catégorie: {property.category.name}</div>
-          <div className="info-line">Surface: {property.surface} m2</div>
-          <div className="info-line">Nombre de pièces: {property.bedroom}</div>
-          <div className="info-line">
-            Détails supplémentaires: {property.features.properties}
-          </div>
-          <div className="info-line">Description: {property.description}</div>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Prix: {property.price} € | Quartier: {property.district.name} | Date d'ajout du bien: {property.createdAt.substring(0, 10)} | 
+          Catégorie: {property.category.name} | Surface: {property.surface} m2 | Nombre de pièces: {property.bedroom} Détails supplémentaires: {property.features.properties}</li>
+        </ul>
         </div>
-      </div>
+      </section>
     );
   }
 }
